5440595dd84dd854669bfe90efa5202c4672b530
Aaron Saunders <aaronksaunders@gmail.com>
2019-12-08 18:03:03 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Ionic from "@ionic/vue";
+import "@ionic/core/css/core.css";
+import "@ionic/core/css/ionic.bundle.css";
 
-Vue.config.productionTip = false
+// router
+import { IonicVueRouter } from "@ionic/vue";
+Vue.use(IonicVueRouter);
+
+Vue.config.productionTip = false;
+Vue.use(Ionic);
+
+const router = new IonicVueRouter({
+  // mode: "history",
+  // base: process.env.BASE_URL,
+  routes: [
+    { path: "/", redirect: "/home" },
+    {
+      path: "/home",
+      name: "home",
+      component: () =>
+        import(/* webpackChunkName: "home" */ "@/components/HomePage"),
+    },
+{
+  path: "/new-item",
+  name: "new-item",
+  component: () =>
+    import(/* webpackChunkName: "new-item" */ "@/components/NewItemPage"),
+}
+  ]
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
